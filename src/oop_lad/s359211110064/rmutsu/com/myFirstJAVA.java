27ff193da7fa3812a858da37488f5870baaa3494
mookchayanis <mook123beet@gmail.com>
2017-11-17 10:50:02 +0700

    Initial commit

---
+++ src/oop_lad/s359211110064/rmutsu/com/myFirstJAVA.java
@@ -0,0 +1,7 @@
+package oop_lad.s359211110064.rmutsu.com;
+
+public class myFirstJAVA {
+    public static void main(String[] args) {
+        System.out.println("HEllo Wordl, I'm mook");
+    }//main
+}//class
