169cad530058d382542f88604f9565f75d731821
mookchayanis <mook123beet@gmail.com>
2017-11-17 11:14:05 +0700

    Successfuly information data

--- src/oop_lad/s359211110064/rmutsu/com/myFirstJAVA.java
+++ src/oop_lad/s359211110064/rmutsu/com/myFirstJAVA.java
@@ -3,5 +3,8 @@ package oop_lad.s359211110064.rmutsu.com;
 public class myFirstJAVA {
     public static void main(String[] args) {
         System.out.println("HEllo Wordl, I'm mook");
+        System.out.println("Information System");
+        System.out.println("Faculty of Management Technololgy");
+        System.out.println("RMUTSV");
     }//main
 }//class
