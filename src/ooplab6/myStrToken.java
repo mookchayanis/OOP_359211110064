fe8c476ec1c10abaf9e0b4d8107acb51370e8ec0
chayanis <mook123beet@gmail.com>
2017-12-29 10:10:40 +0700

    myStrToken

---
+++ src/ooplab6/myStrToken.java
@@ -0,0 +1,20 @@
+package ooplab6;
+
+import java.util.StringTokenizer;
+
+public class myStrToken {
+    public static void main (String[]args){
+        //StringTokenizer
+        String msg = "Hello Welcom MT RMUTSV 2017";
+        StringTokenizer myToken = new StringTokenizer(msg);
+        //countToken()
+        System.out.println(myToken.countTokens());
+        while (myToken.hasMoreTokens()){
+            StringBuffer buffer = new StringBuffer (myToken.nextToken());
+            System.out.print(buffer.reverse()+" ");
+        }//while
+
+
+
+    }//main
+}//class
