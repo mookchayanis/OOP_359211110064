8bc12641182a5f3e82685356831d0bf242317c9f
chayanis <mook123beet@gmail.com>
2017-12-29 09:35:15 +0700

    StringCompare

---
+++ src/ooplab6/StringCompare.java
@@ -0,0 +1,30 @@
+package ooplab6;
+
+public class StringCompare {
+    public static void main (String[] args){
+        String str1 = "Hello";
+        String str2 = "Helloooo";
+        //compare String
+        //type 1 (==) T/F
+        if (str1 == str2)System.out.println("true");
+        else System.out.println("false");
+        //type 2 (equals()mehtod)
+        if (str1.equals(str2) )System.out.println("true");
+        else System.out.println("false");
+        //type3 (compareTo() mehtod)
+        //0 = String is equal.
+        //- = Left String is less than Right String
+        //+ = Left String is more than Right String
+        if (str1.compareTo(str2)==0)
+            System.out.println("true");
+        else if (str1.compareTo(str2)>=1)
+            System.out.println("str1 is more than str2");
+        else
+            System.out.println("str1 is less than str2");
+
+
+
+
+
+    }//main
+}//class
